c7932725735f448546fe3feef703fda46b0a15d4
Brxnni <arne.norvin@gmail.com>
2023-06-19 12:16:26 +0200

    🐛 Fix bug where settings are overwritten on updating or rrefreshing the extension

--- src/background.js
+++ src/background.js
@@ -1,8 +1,7 @@
 // Add standard settings when none are there yet
-chrome.runtime.onInstalled.addListener(() => {
-	chrome.storage.sync.set({
-		size: "1.2em",
-		margin: "0 0.075em 0 0.075em",
-		style: "twitter"
-	});
+chrome.runtime.onInstalled.addListener(async () => {
+	let storage = await chrome.storage.sync.get();
+	if (!storage.margin) chrome.storage.sync.set({ margin: "0 0 0.075em 0 0.075em" });
+	if (!storage.size) chrome.storage.sync.set({ size: "1.2em" });
+	if (!storage.style) chrome.storage.sync.set({ style: "twitter" });
 });
